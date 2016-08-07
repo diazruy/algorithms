4f757aa78e0d80c3f9f925e45c9add9ffbbeceeb
Ruy Diaz <diazruy@gmail.com>
2016-08-07 13:21:02 -0700

    Refactor Graph and add #numVertices and #outDegrees

--- spec/data_structures/graph_spec.js
+++ spec/data_structures/graph_spec.js
@@ -1,13 +1,31 @@
 var Graph = require('../../src/data_structures/graph.js');
 
 describe('Graph', function(){
-  it('builds the graph from the supplied edges', function(){
+  var graph;
+
+  beforeEach(function() {
     var edges = [
       ['a', 'b'],
       ['a', 'c'],
       ['b', 'c']
     ];
-    var graph = new Graph(edges);
+    graph = new Graph();
+    graph.addEdges(edges);
+  });
+
+  it('builds the graph from the supplied edges', function(){
     expect(Object.keys(graph.vertices).sort()).toEqual(['a', 'b', 'c']);
+  });
+
+  describe('numVertices', function(){
+    it('returns the total number of vertices', function() {
+      expect(graph.numVertices).toEqual(3);
+    });
+  });
+
+  describe('outDegree', function(){
+    it('returns the number of edges connecting to a vertex', function(){
+      expect(graph.outDegree('a')).toEqual(2);
+    });
   });
 });
--- src/data_structures/graph.js
+++ src/data_structures/graph.js
@@ -5,31 +5,45 @@ var Edge = function(y, weight) {
   };
 };
 
-var Graph = function(edges, isDirected) {
+var Graph = function(isDirected) {
   if(typeof isDirected == 'undefined') isDirected = false;
-  var x, y, edge, vertices;
-  vertices = {};
-  edges.forEach(function(edgePair){
-    x = edgePair[0];
-    y = edgePair[1];
-    Graph.addEdge(vertices, x, y, isDirected);
-  });
-  return {
-    numVertices: 0,
-    numEdges: 0,
-    vertices: vertices
-  };
+  this.numVertices = 0;
+  this.numEdges = 0;
+  this.vertices = {};
+  this.outDegrees = {};
+  this.isDirected = isDirected;
 };
 
-Graph.addEdge = function(vertices, x, y, isDirected) {
-  var edge = new Edge(y);
-  if(vertices[x]) {
-    edge.next = vertices[x];
+Graph.prototype = {
+  addEdges: function(edges) {
+    var x, y;
+    edges.forEach(function(edgePair){
+      x = edgePair[0];
+      y = edgePair[1];
+      this.addEdge(x, y);
+    }, this);
+  },
+
+  addEdge: function(x, y, isDirected) {
+    var edge = new Edge(y);
+    if(this.vertices[x]) {
+      edge.next = this.vertices[x];
+    } else {
+      this.numVertices++;
+    }
+    if(typeof this.outDegrees[x] == 'undefined'){
+      this.outDegrees[x] = 0;
+    }
+    this.outDegrees[x]++;
+    this.vertices[x] = edge;
+    if(!isDirected) {
+      this.addEdge(y, x, true);
+    }
+  },
+
+  outDegree: function(vertex) {
+    return this.outDegrees[vertex];
   }
-  vertices[x] = edge;
-  if(!isDirected) {
-    Graph.addEdge(vertices, y, x, true);
-  }
-};
+}
 
 module.exports = Graph;
